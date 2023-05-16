36b4a2d7669a77c8387536f860cab9026eb4d2ef
CameronGregoire <cameron.gregoire2001@gmail.com>
2023-05-16 16:32:21 -0700

    Super basic outline of load.

--- src/view/LoadFrame.java
+++ src/view/LoadFrame.java
@@ -14,8 +14,6 @@ public class LoadFrame extends JFrame implements GUIFrame{
 
     private void start() {
         JPanel mainPanel;
-        JButton loadButton;
-        JButton cancelButton;
 
         final double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         final double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
@@ -26,8 +24,44 @@ public class LoadFrame extends JFrame implements GUIFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        setLayout(new GridBagLayout());
+        createCancelButton();
+        createLoadButton();
+
+        // Create the main panel and add the button panel
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(BACKGROUND_COLOR);
+        mainPanel.setForeground(FOREGROUND_COLOR);
+
+        // Add the main panel to the frame
+        add(mainPanel);
+
+        setVisible(true);
+    }
+
+    private void createLoadButton(){
+        JButton loadButton = new JButton("Load");
+        loadButton.setPreferredSize(new Dimension(100, 25));
+        loadButton.setFont(BODY_FONT);
+        loadButton.setBackground(FOREGROUND_COLOR);
+        loadButton.setForeground(BACKGROUND_COLOR);
+
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        gbc2.gridx = GridBagConstraints.RELATIVE;
+        gbc2.gridy = GridBagConstraints.RELATIVE;
+        gbc2.anchor = GridBagConstraints.SOUTHEAST;
+        gbc2.weightx = 1.0;
+        gbc2.weighty = 1.0;
+        gbc2.insets = new Insets(10, 10, 10, 10);
+
+        add(loadButton, gbc2);
+
+    }
+
+    private void createCancelButton(){
         // Create the Cancel and Load buttons
-        cancelButton = new JButton("Cancel");
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setPreferredSize(new Dimension(100, 25));
         cancelButton.setFont(BODY_FONT);
         cancelButton.setBackground(FOREGROUND_COLOR);
         cancelButton.setForeground(BACKGROUND_COLOR);
@@ -42,29 +76,14 @@ public class LoadFrame extends JFrame implements GUIFrame{
                 dispose();
             }
         });
-
-        loadButton = new JButton("Load");
-        loadButton.setFont(BODY_FONT);
-        loadButton.setBackground(FOREGROUND_COLOR);
-        loadButton.setForeground(BACKGROUND_COLOR);
-
-        // Create a panel for the buttons
-        JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 15, 5));
-        buttonPanel.setBackground(BACKGROUND_COLOR);
-        buttonPanel.setForeground(FOREGROUND_COLOR);
-        buttonPanel.add(cancelButton);
-        buttonPanel.add(loadButton);
-
-        // Create the main panel and add the button panel
-        mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setBackground(BACKGROUND_COLOR);
-        mainPanel.setForeground(FOREGROUND_COLOR);
-        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-        // Add the main panel to the frame
-        add(mainPanel);
-
-        setVisible(true);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.anchor = GridBagConstraints.SOUTHWEST;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        add(cancelButton, gbc);
     }
 
     public static void main(String[] args) {
