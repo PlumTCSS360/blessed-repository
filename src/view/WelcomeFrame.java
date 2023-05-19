43a1b8c7ccc3279d07927cd6ad6e5a0aa484251e
Jiameng Li <banbaiyanaito@icloud.com>
2023-05-18 22:40:33 -0700

    Method for export user info and projects.

--- src/view/WelcomeFrame.java
+++ src/view/WelcomeFrame.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
